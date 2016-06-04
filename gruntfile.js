8e3df5f7183d227d83e03a74b42c3e3e37f095c7
Maxime Bonhomme <maxime@neverbland.com>
2016-06-04 21:37:01 +0100

    gitignore and grunt

---
+++ gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    grunt.settings = {
+        name: 'bonhomme-build',
+        distDir: 'dist/',
+        devDir: 'dev/',
+        fileExt: '.html'
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+        },
+        browserSync: {
+            options: {
+                watchTask: true,
+                server: './' + grunt.settings.devDir
+            },
+            dev: {
+                bsFiles: {
+                    src : [
+                        grunt.settings.devDir + 'css/{,**/}*.css',
+                        grunt.settings.devDir + '*.html',
+                    ]
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
+                    grunt.settings.devDir + 'js/custom/main.js'
+                ],
+                dest: grunt.settings.distDir + 'js/videoLabel.js'
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                src: grunt.settings.distDir + 'js/videoLabel.js',
+                dest: grunt.settings.distDir + 'js/videoLabel.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', [
+        'browserSync:dev',
+        'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'concat:dist',
+        'uglify:dist'
+    ]);
+
+};
